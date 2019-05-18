4c7b8ecd1c3fcfc2f5bac915f91ac4527513f0ff
TanselArif-21 <tanselarif@live.co.uk>
2019-05-18 16:43:23 +0100

    Working hosted app

--- app.py
+++ app.py
@@ -1,39 +1,26 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify, render_template
+from flask import Flask, request, jsonify
 from functions import return_something, return_something2, get_prediction_lr
-import flask_excel as excel
 import pandas as pd
-import os
-import string
 import random
+import string
 
+app=Flask(__name__)
 
-
-# This line sets the app directory as the working directory
-app = Flask(__name__)
-
-# The home route
-@app.route('/', methods=['GET'])
-def home_page():
-    # Show the index page
-    return render_template('index.html')
-
-# A route to the test page that simply returns hello
-@app.route('/plot', methods=['GET'])
-def plot():
-    target = os.path.join(APP_ROOT, 'static')
-    print(target)
-    return render_template("plot.html")
-
-# A route to the test page that simply returns hello
-@app.route('/hello', methods=['GET'])
-def hello():
-    return 'Hello, World!'
-
-# The dosomething route
-@app.route('/dosomething', methods=['GET'])
-def dosomething():
-    # Show something
-    return str(return_something())
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        data_xls = pd.read_csv(f)
+        return data_xls.to_html()
+        #return "anna"
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    '''
 
 # The dosomething2 route
 @app.route('/dosomething2', methods=['GET', 'POST'])
@@ -41,20 +28,8 @@ def dosomething2():
     # If the request is a post request
     if request.method == 'POST':
         # This line creates a dataframe from a list of lists
-        df = pd.DataFrame(request.args.get_array(field_name='file'))
-
-        #df = pd.read_excel(f)
-        # The first row is the list of column names so set the column names to the first row
-        df.columns = df.iloc[0, :]
-
-        # Now remove the first row
-        df = df[1:]
-
-        # Print the dataframe to the console
-        print(df)
-
-        # Print the column names to the console
-        print(df.columns)
+        f = request.files['file']
+        df = pd.read_csv(f)
 
         # These are the values given for the fields
         input_list = list(request.form.values())
@@ -63,28 +38,69 @@ def dosomething2():
         for i in range(4):
             filename = filename + random.choice(string.ascii_letters)
 
-        # These are the names of the fields, i.e. LotFrontage, LotArea etc...
+        # # These are the names of the fields, i.e. LotFrontage, LotArea etc...
         input_names = list(request.form)
 
         result = get_prediction_lr(df, input_list, filename)
 
-        # This code section creates a dictionary where each key is a row in the dataframe
+        # # This code section creates a dictionary where each key is a row in the dataframe
         res = dict()
         for i in range(len(df)):
             res[i] = list(df.iloc[i, :])
 
-        # Return the result
-        #return 'The predicted Sale Price of this house is: ' + str(round(result, 2)) + '. \n <button><a href="/plot/' + filename + '">See Sales Price Distribution</a></button><p></p>'
+        # # Return the result
+        # #return 'The predicted Sale Price of this house is: ' + str(round(result, 2)) + '. \n <button><a href="/plot/' + filename + '">See Sales Price Distribution</a></button><p></p>'
         return 'The predicted Sale Price of this house is: ' + str(round(result, 2)) + '. <p></p> <img src="static/' + filename + '.png"><p></p>'
     else:
         # Show the form page
-        return render_template('dosomethingform.html')
+        return '''
+        <!doctype html>
+        <title>Upload an excel file</title>
+        <h1>Excel file upload (csv)</h1>
+        <form action="" method=post enctype=multipart/form-data><p></p>
+    	<label>LotFrontage</label><input type="text" name="LotFrontage">
+    	<label>LotArea</label><input type="text" name="LotArea"><p></p>
+    	<label>OverallQual</label><input type="text" name="OverallQual">
+    	<label>OverallCond</label><input type="text" name="OverallCond"><p></p>
+    	<label>YearBuilt</label><input type="text" name="YearBuilt">
+    	<label>YearRemodAdd</label><input type="text" name="YearRemodAdd"><p></p>
+    	<label>MasVnrArea</label><input type="text" name="MasVnrArea">
+    	<label>BsmtFinSF1</label><input type="text" name="BsmtFinSF1"><p></p>
+    	<label>BsmtFinSF2</label><input type="text" name="BsmtFinSF2">
+    	<label>BsmtUnfSF</label><input type="text" name="BsmtUnfSF"><p></p>
+    	<label>TotalBsmtSF</label><input type="text" name="TotalBsmtSF">
+    	<label>1stFlrSF</label><input type="text" name="1stFlrSF"><p></p>
+    	<label>2ndFlrSF</label><input type="text" name="2ndFlrSF">
+    	<label>LowQualFinSF</label><input type="text" name="LowQualFinSF"><p></p>
+    	<label>GrLivArea</label><input type="text" name="GrLivArea">
+    	<label>BsmtFullBath</label><input type="text" name="BsmtFullBath"><p></p>
+    	<label>BsmtHalfBath</label><input type="text" name="BsmtHalfBath">
+    	<label>FullBath</label><input type="text" name="FullBath"><p></p>
+    	<label>HalfBath</label><input type="text" name="HalfBath">
+    	<label>BedroomAbvGr</label><input type="text" name="BedroomAbvGr"><p></p>
+    	<label>KitchenAbvGr</label><input type="text" name="KitchenAbvGr">
+    	<label>Fireplaces</label><input type="text" name="Fireplaces"><p></p>
+    	<label>GarageYrBlt</label><input type="text" name="GarageYrBlt">
+    	<label>GarageCars</label><input type="text" name="GarageCars"><p></p>
+    	<label>GarageArea</label><input type="text" name="GarageArea">
+    	<label>WoodDeckSF</label><input type="text" name="WoodDeckSF"><p></p>
+    	<label>OpenPorchSF</label><input type="text" name="OpenPorchSF">
+    	<label>EnclosedPorch</label><input type="text" name="EnclosedPorch"><p></p>
+    	<label>3SsnPorch</label><input type="text" name="3SsnPorch">
+    	<label>ScreenPorch</label><input type="text" name="ScreenPorch"><p></p>
+    	<label>PoolArea</label><input type="text" name="PoolArea">
+    	<label>MiscVal</label><input type="text" name="MiscVal"><p></p>
+    	<label>MoSold</label><input type="text" name="MoSold">
+    	<label>YrSoldlabel</label><input type="text" name="YrSold"><p></p>
+    	<label>SalePrice</label><label>To be predicted</label>
+    	<input type=file name=file><input type=submit value=Upload>
+        </form>
+        '''
 
-if __name__ == '__main__':
-    # Initiate the excel part of flask
-    excel.init_excel(app)
-    # Let the console know that the load is successful
-    print("loaded OK")
 
-    # Set to debug mode
-    app.run(debug=True)
+@app.route("/export", methods=['GET'])
+def export_records():
+    return
+
+if __name__ == "__main__":
+    app.run()
--- functions.py
+++ functions.py
@@ -23,29 +23,29 @@ def get_prediction_lr(df, ls, filename):
     df: a data frame
     ls: a list containing the information for a house to predict for
     '''
-
+    #return(df.to_html())
     # Save a plot image
     plot_df(df['SalePrice'], filename)
 
-    # This is a linear regression object
+    # # This is a linear regression object
     linreg = LinearRegression()
 
-    # This is our predictors dataframe
+    # # This is our predictors dataframe
     x = df.drop('SalePrice', axis=1)
 
-    # We fit the model using the predictors
+    # # We fit the model using the predictors
     linreg.fit(x, df['SalePrice'])
 
-    # This is our observation we would like to predict. Initially we use the means of all observations
-    # and we overwrite the fields which are provided by the user. i.e. the default of a field
-    # is the mean for that field
+    # # This is our observation we would like to predict. Initially we use the means of all observations
+    # # and we overwrite the fields which are provided by the user. i.e. the default of a field
+    # # is the mean for that field
     pred_x = x.mean().values.reshape(1, -1)
 
-    # Loop through the user provided list of fields and update the list for our observation
-    # whenever a field is supplied
+    # # Loop through the user provided list of fields and update the list for our observation
+    # # whenever a field is supplied
     for i in range(len(ls)):
         if ls[i] != '':
             pred_x[0][i] = float(ls[i])
 
-    # Return the predicted Sale Price
+    # # Return the predicted Sale Price
     return list(linreg.predict(pred_x))[0]
